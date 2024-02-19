f1df010f5b5c6c877f82b64aaadd41bb38eafea5
MLankaoui <marouanelankaoui66@gmail.com>
2024-02-19 11:37:52 +0100

    sigint implemented and betty handled

---
+++ sigint__hand__ling.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* siginthandl__ing - Signal handler for SIGINT.
+* @signum: The signal number.
+* @bufff_eerr: Pointer to the buffer to be freed.
+*/
+void siginthandl__ing(int signum, char *bufff_eerr)
+{
+    (void)signum;
+    printf("\n");
+    free(bufff_eerr);
+    exit(EXIT_SUCCESS);
+}
