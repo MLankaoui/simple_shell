f2abb96dae85442dcbf7f90e890384f3610850cc
Fatima20027 <fatima.hailou@icloud.com>
2024-02-19 11:35:15 +0100

    some standard lib were added to main.h

--- main.h
+++ main.h
@@ -5,6 +5,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <ctype.h>
 
 void create_pro__mpt(char **buff_err, size_t *buff_si_zee);
 
