0d61afd270bdc8250c1b535ce795ce2322911565
Fatima20027 <fatima.hailou@icloud.com>
2024-02-19 11:28:38 +0100

    pro__mpt_geen__erat_ion.c was updated

--- pro__mpt_geen__erat_ion.c
+++ pro__mpt_geen__erat_ion.c
@@ -1,27 +1,27 @@
 #include "main.h"
 
 /**
-* generate_prompt - Generates and handles the shell prompt.
-* @buffer: Pointer to the buffer to store user input.
-* @buffer_size: Pointer to the size of the input buffer.
+* create_pro__mpt - Generates and handles the shell prompt.
+* @buff_err: Pointer to the buffer to store user input.
+* @buff_si_zee: Pointer to the size of the input buffer.
 */
-void generate_prompt(char **buffer, size_t *buffer_size)
+void create_pro__mpt(char **buff_err, size_t *buff_si_zee)
 {
-	int nread;
+	int num_read;
 
-	struct sigaction sa;
-	sa.sa_handler = SIG_IGN;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = 0;
-	sigaction(SIGINT, &sa, NULL);
+	struct sigaction ld;
+	ld.ld_handler = SIG_IGN;
+	sigemptyset(&ld.ld_mask);
+	ld.ld_flags = 0;
+	sigaction(SIGINT, &ld, NULL);
 
-	nread = custom_getl__iiine(buffer, buffer_size, stdin);
-	if (nread != -1)
+	num_read = mod_get_liine(buff_err, buff_si_zee, stdin);
+	if (num_read != -1)
 	{
-		sa.sa_handler = SIG_DFL;
-		sigaction(SIGINT, &sa, NULL);
-		(*buffer)[nread - 1] = '\0';
-		is_command_available(*buffer);
+		ld.ld_handler = SIG_DFL;
+		sigaction(SIGINT, &ld, NULL);
+		(*buff_err)[num_read - 1] = '\0';
+		command_ex_ists(*buffer);
 	}
 	else
 	{
