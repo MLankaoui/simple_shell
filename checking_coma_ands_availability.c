19a8e9cc2a46f36a6ecbc573d9412b477e66aeba
MLankaoui <marouanelankaoui66@gmail.com>
2024-02-20 16:38:47 +0100

    command availability optimisation

--- checking_coma_ands_availability.c
+++ checking_coma_ands_availability.c
@@ -4,16 +4,14 @@
  * checks_co_mmand_availability - Checks if a command is available.
  * @cmd: The command to check.
  */
-void checks_co_mmand_availability(char *cmd)
+void checks_co_mmand_availability(char *comma__and__s)
 {
 
 	char complete_path[256];
 
 	struct stat st;
 
-	snprintf(complete_path, sizeof(complete_path), "/bin/%s", cmd);
+	snprintf(complete_path, sizeof(complete_path), "/bin/%s", comma__and__s);
 	if (stat(complete_path, &st) == 0)
-	{
-		parse_handle_input(cmd);
-	}
+		parse_handle_input(comma__and__s);
 }
