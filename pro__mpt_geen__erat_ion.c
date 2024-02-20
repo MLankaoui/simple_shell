93f6721fe9b1a1b3dd3eece0cf655f2eb8d4714d
MLankaoui <marouanelankaoui66@gmail.com>
2024-02-20 15:01:29 +0100

    quick fix

--- pro__mpt_geen__erat_ion.c
+++ pro__mpt_geen__erat_ion.c
@@ -15,23 +15,23 @@ void create_pro__mpt(char **buff_err, size_t *buff_si_zee)
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = 0;
 	sigaction(SIGINT, &sa, NULL);
-	num_read = custom_getl__iiine(buff_err, buff_si_ze, stdin);
+	num_read = custom_getl__iiine(buff_err, buff_si_zee, stdin);
 
 	if (num_read != -1)
 	{
 		sa.sa_handler = SIG_DFL;
 		sigaction(SIGINT, &sa, NULL);
-	}
 
-	if (num_read == 1 && isatty(STDIN_FILENO))
-	{
-		printf("\nEOF detected. Exiting shell.\n");
-		free(buff_err);
-		exit(EXIT_SUCCESS);
-	}
 
-	buff_err[num_read - 1] = '\0';
-	parse_user_input(*buff_err);
+		if (num_read == 1 && isatty(STDIN_FILENO))
+		{
+			printf("\nEOF detected. Exiting shell.\n");
+			free(buff_err);
+			exit(EXIT_SUCCESS);
+		}
+
+		buff_err[num_read - 1] = '\0';
+		parse_user_input(*buff_err);
 	}
 	else
 		exit(EXIT_SUCCESS);
