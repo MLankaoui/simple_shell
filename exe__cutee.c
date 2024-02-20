bef1c573cf2a295f8a7aa5ab53cb73867e17a56f
MLankaoui <marouanelankaoui66@gmail.com>
2024-02-20 16:41:22 +0100

    some code optimisation

--- exe__cutee.c
+++ exe__cutee.c
@@ -1,21 +1,24 @@
 #include "main.h"
 
 /**
- * parseHandle_input - Parses and handles input.
- * @buff_err: The input buffer containing the command and arguments.
- */
+* parseHandle_input - Parses and handles input.
+* @buff_err: The input buffer containing the command and arguments.
+*/
 void pa_rseHandle_input(char *buff_err)
 {
 
-	char *args[MAX_ARGS], *token;
+	char *args[MAX_ARGS], *tokkeee__nnn;
 
-	int argc = 0;
+	int index = 0;
 
-	for (token = strtok(buff_err, " \t\n"); token != NULL && argc < MAX_ARGS - 1; token = strtok(NULL, " \t\n"))
+	tokkeee__nnn = strtok(buff_err, " \t\n");
+	for (tokkeee__nnn != NULL && index < MAX_ARGS - 1)
 	{
-		args[argc++] = token;
+		args[index++] = tokkeee__nnn;
+		tokkeee__nnn = strtok(NULL, " \t\n")
 	}
 
-	args[argc] = NULL;
+
+	args[index] = NULL;
 	exec_cmd(args);
 }
