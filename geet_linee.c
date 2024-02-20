1145194072eecce88d03e71be1d973dd49a7b7b4
MLankaoui <marouanelankaoui66@gmail.com>
2024-02-20 16:03:33 +0100

    betty fixed in getline.c

--- geet_linee.c
+++ geet_linee.c
@@ -11,14 +11,13 @@
 */
 int read_geet_linee(char **line__buff, size_t *bsize, FILE *input_str_eam)
 {
-	int br = 0;
+	int br = 0, chr;
+
 	size_t total_br = 0;
-	int chr;
 	char *newLine__buff;
 
 	if (line__buff == NULL || bsize == NULL || input_str_eam == NULL)
 		return (-1);
-
 	if (*line__buff == NULL || *bsize == 0)
 	{
 		*bsize = 128;
@@ -26,38 +25,29 @@ int read_geet_linee(char **line__buff, size_t *bsize, FILE *input_str_eam)
 		if (*line__buff == NULL)
 			return (-1);
 	}
-
 	while ((chr = fgetc(input_str_eam)) != EOF)
 	{
 		if (total_br >= *bsize - 1)
-	{
-
-	*bsize *= 2;
-	newLine__buff = (char *)realloc(*line__buff, *bsize);
-
-	if (newLine__buff == NULL)
-	{
-		free(*line__buff);
-		return (-1);
-	}
-
-	*line__buff = newLine__buff;
-	}
-
+		{
+			*bsize *= 2;
+			newLine__buff = (char *)realloc(*line__buff, *bsize);
+			if (newLine__buff == NULL)
+			{
+				free(*line__buff);
+				return (-1);
+			}
+			*line__buff = newLine__buff;
+		}
 		(*line__buff)[total_br++] = (char)chr;
 		br++;
-
 		if (chr == '\n')
-		break;
-    }
-
-	if (total_br == 0 && br == 0) 
+			break;
+	}
+	if (total_br == 0 && br == 0)
 	{
 		free(*line__buff);
 		return (-1);
 	}
-
 	(*line__buff)[total_br] = '\0';
-
 	return (total_br);
 }
