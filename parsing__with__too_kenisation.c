4c5e5ffda8861e4a273a91dae783ec9f7757d4fa
MLankaoui <marouanelankaoui66@gmail.com>
2024-02-20 16:50:54 +0100

    fixing syntaxe issues

---
+++ parsing__with__too_kenisation.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* pa_rseHandle_input - Parses and handles input.
+* @buff_err: The input buffer containing the command and arguments.
+*/
+void pa_rseHandle_input(char *buff_err)
+{
+
+	char *args[MAX_ARGS], *tokkeee__nnn;
+
+	int index;
+
+	tokkeee__nnn = strtok(buff_err, " \t\n");
+	for (index = 0; tokkeee__nnn != NULL && index < MAX_ARGS - 1;)
+	{
+		args[index++] = tokkeee__nnn;
+		tokkeee__nnn = strtok(NULL, " \t\n");
+	}
+
+
+	args[index] = NULL;
+	exec_cmd(args);
+}
